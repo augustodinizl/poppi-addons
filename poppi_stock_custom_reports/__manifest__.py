3a6bb4b0595f9d1f14f72a846122bf91692aa117
augustodinizl <augusto.diniz.l@gmail.com>
2023-07-15 19:16:03 -0300

    test1

--- poppi_stock_custom_reports/__manifest__.py
+++ poppi_stock_custom_reports/__manifest__.py
@@ -4,15 +4,27 @@
 {
     'name': 'Poppi Stock Custom Reports',
     'summary': """
+<<<<<<< HEAD
         Custom QWEB reports for Poppi: "Minuta de transporte" and "Etiqueta de Pacote"""",
+=======
+        Custom QWEB reports for Poppi: 'Minuta de transporte' and 'Etiqueta de Pacote'""",
+>>>>>>> 4e30721 (test1)
     'version': '14.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Escodoo',
     'website': 'https://www.escodoo.com.br',
     'depends': [
+        'stock',
+        'l10n-br'
     ],
     'data': [
+        'report/etiqueta.xml',
+        'report/minuta.xml'
     ],
-    'demo': [
-    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'auto_install': False,
+
 }
