0e6539779c8f71596c5a2720567f0ff8baa75204
augustodinizl <augusto.diniz.l@gmail.com>
2023-07-15 15:46:47 -0300

    initial pull

---
+++ poppi_stock_custom_reports/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Poppi Stock Custom Reports',
+    'summary': """
+        Custom QWEB reports for Poppi: "Minuta de transporte"and Ëtiqueta de Pacote"""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Escodoo',
+    'website': 'https://www.escodoo.com.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
